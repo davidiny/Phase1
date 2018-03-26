21939acb65bc691767303204f8468e892c3db54f
David Inyangson <dinyangs@andrew.cmu.edu>
2018-03-26 22:23:12 +0000

    Created tests for card model

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -8,4 +8,33 @@ class CardTest < ActiveSupport::TestCase
   should validate_presence_of(:card_text)
   should validate_presence_of(:act)
   should validate_presence_of(:type)
+  should validate_numericality_of(:act)
+  
+  
+  should allow_value("ABCDEFG").for(:card_text)
+  should allow_value("abcdefg hijklmnop qrstu vqxyz").for(:instructions)
+  should allow_value("abcdefg hijklmnop qrstu vqxyz").for(:alert_text)
+  should allow_value("complicity").for(:type)
+  should allow_value(2).for(:act)
+  should allow_value(0).for(:act)
+  should_not allow_value(-1).for(:act)
+  
+  
+  should_not allow_value(nil).for(:card_text)
+  should_not allow_value(nil).for(:act)
+  should_not allow_value(nil).for(:type)
+  should allow_value(nil).for(:instructions)
+  should allow_value(nil).for(:alert_text)
+  
+  # set up context
+  context "Within context" do
+    setup do 
+      create_cards
+    end
+    
+    teardown do
+      delete_cards
+    end
+  end
+
 end
